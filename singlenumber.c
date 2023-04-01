ebdc2da0b5e085984c817440e411fba85631be69
HueZhang <1281762297@qq.com>
2023-04-02 02:35:25 +0800

    单个数字，增加大神做法，使用异或预算

--- singlenumber.c
+++ singlenumber.c
@@ -21,11 +21,23 @@ int singlenumber(int *nums,int numsSize)
 	return -1;
 }
 
+int singlenumber2(int *nums,int numSize)
+{
+	// 大神做法
+	// 使用位运算
+	// 异或运算，相同为0,不同为1
+	int res = 0;
+	for (int i=0;i < numSize;i++)
+	{
+		res = res ^ nums[i];
+	}
+	return res;
+}
 
 
 int main()
 {
 	int nums[7] = {2,2,1,1,3,3,6};
-	int ans = singlenumber(nums,7);
+	int ans = singlenumber2(nums,7);
 	printf("%d\n",ans);	
 }
